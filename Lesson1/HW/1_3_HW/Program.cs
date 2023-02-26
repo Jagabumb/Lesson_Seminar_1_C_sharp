babacb4e424a1451654fff89e579f08b6bf52dc9
Vadim <jagabumba@gmail.com>
2023-02-26 20:06:19 +0700

    task 1_2 done rework

---
+++ Lesson1/HW/1_3_HW/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine(a % b);
+
+
+//Console.WriteLine("Hello, World!");
