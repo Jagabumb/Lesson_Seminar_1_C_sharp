381cfa90455affc1273ebaf6cb6cc6c309eecefb
Vadim <jagabumba@gmail.com>
2023-02-26 20:35:19 +0700

    process

--- Lesson1/HW/1_4_HW/Program.cs
+++ Lesson1/HW/1_4_HW/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int a = int.Parse(Console.ReadLine()!);
+int b = 1;
+while (a >= b)
+{
+  Console.Write($"{b} ");
+  b += 1; //счетчик плюс 1
+}
+
+
+//Console.WriteLine(a);
