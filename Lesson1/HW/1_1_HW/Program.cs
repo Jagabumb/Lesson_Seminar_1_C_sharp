fb0f4dca89625066f7e12a4ac511cc1f80472ca2
Vadim <jagabumba@gmail.com>
2023-02-26 18:34:36 +0700

    HW 1_1

--- Lesson1/HW/1_1_HW/Program.cs
+++ Lesson1/HW/1_1_HW/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a < b)
+{
+  Console.WriteLine("Ваш ответ");
+  Console.WriteLine(b);
+}
+else if (a > b)
+{
+  Console.WriteLine("Ваш ответ");
+  Console.WriteLine(a);
+}
+else
+{
+  Console.WriteLine("Error");
+}
+
+
+Console.WriteLine("Всего доброго!");
